1951af51540b01d10620a89bad221dcc8066011e
nabarabane <nabarabane@yandex.ru>
2015-07-21 13:13:50 +0300

    no message

--- src/Handler.php
+++ src/Handler.php
@@ -17,10 +17,29 @@ class Handler
 		$this->access_token = trim(file_get_contents($file));
 	}
 
-	public function request(Request $request)
+	public function request($method = null, $params = null)
 	{
+		if (empty($method)) {
+			throw new \Exception('Не указан метод запроса');
+		}
+
+		if (empty($params) || !is_array($params)) {
+			throw new \Exception('Не указаны параметры запроса');
+		}
+
+		$url = $method;
+
+		if (count($params)) {
+			$i = 1;
+			foreach ($params as $key => $value) {
+				$url .= ($i == 1) ? '?' : '&';
+				$url .= $key.'='.$value;
+				$i++;
+			}
+		}
+
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'https://api.vk.com/method/'.$request->url);
+		curl_setopt($ch, CURLOPT_URL, 'https://api.vk.com/method/'.$url);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -39,12 +58,11 @@ class Handler
 		$this->result = json_decode($result);
 
 		if (!empty($this->result->error)) {
-			throw $this->catchError();
+			throw new \Exception('Code: ('.$this->result->error->error_code.') '.$this->result->error->error_msg);
 		}
-	}
 
-	private function catchError()
-	{
-		return new \Exception('Code: ('.$this->result->error->error_code.') '.$this->result->error->error_msg);
+		$this->result = $this->result->response;
+
+		return $this;
 	}
 }
